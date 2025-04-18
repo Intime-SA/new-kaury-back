7ded31fed2fbc8ab9adb31f2ade3dd77f451ba84
Intime-SA <invertimesa@gmail.com>
2025-04-18 09:21:13 -0300

    .env

--- app/services/orders.service.ts
+++ app/services/orders.service.ts
@@ -1,7 +1,7 @@
 import { Order } from "@/types/orders"
 import { OrderStatusType } from "../components/OrderStatus"
 
-const API_BASE_URL = "http://localhost:3000/api"
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 interface GetOrdersParams {
   status?: OrderStatusType
